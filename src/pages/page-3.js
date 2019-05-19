8ac369debe4a6072cd06db7c9059423943d55f48
Joseph <z@charmed.tech>
2019-05-19 11:42:53 -0700

    Add sass, more components and styling

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -4,17 +4,17 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const ThirdPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Page three" />
+    <h1>Hi from the third page</h1>
+    <p>Welcome to page 3</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />
-    <Link to="/page-3/">Go to the third page</Link>
+    <Link to="/page-2/">Go to the second page</Link>
     <br />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default ThirdPage
